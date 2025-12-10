caa712aeda5c8a9feabdef92abb76ee8035eb56e
Nagender Naroju <nagender5311naroju@gmail.com>
2025-12-10 19:43:01 +0530

    pusher setup in the .env file & created migration for messages table

---
+++ database/migrations/2025_12_10_141057_create_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender_id');
+            $table->text('body');
+            $table->json('meta')->nullable(); // store metadata added by job
+            $table->boolean('processed')->default(false);
+            $table->timestamps();
+
+            // add index if you need
+            $table->index('sender_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
